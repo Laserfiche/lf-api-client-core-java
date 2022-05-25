4f86a21a1af5256c749a52dab3365730afa9e1c8
Jason Jia <jason.jia@laserfiche.com>
2022-05-25 09:39:35 -0400

    Add interface for token api

--- src/main/java/com/laserfiche/api/client/oauth/TokenApiClient.java
+++ src/main/java/com/laserfiche/api/client/oauth/TokenApiClient.java
@@ -4,7 +4,14 @@ import com.laserfiche.api.client.model.AccessKey;
 import com.laserfiche.api.client.model.GetAccessTokenResponse;
 
 public interface TokenApiClient {
-    GetAccessTokenResponse getAccessToken(String servicePrincipalKey, AccessKey accessKey) {
 
-    }
+    /**
+     * Gets an access token given the service principal key and the app access key.
+     * These values can be exported from the Laserfiche Developer Console.
+     * This is the client credentials flow that applies to service applications.
+     * @param servicePrincipalKey
+     * @param accessKey
+     * @return GetAccessTokenResponse
+     */
+    GetAccessTokenResponse getAccessToken(String servicePrincipalKey, AccessKey accessKey);
 }
