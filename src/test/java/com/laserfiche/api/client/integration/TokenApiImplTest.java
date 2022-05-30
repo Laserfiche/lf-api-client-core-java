24212b3c39eb606dfd197117a44aa7b56c922bc3
Jason Jia <jason.jia@laserfiche.com>
2022-05-30 16:05:07 -0400

    Add skeleton for integration tests

--- src/test/java/com/laserfiche/api/client/integration/TokenApiImplTest.java
+++ src/test/java/com/laserfiche/api/client/integration/TokenApiImplTest.java
@@ -1,4 +1,18 @@
 package com.laserfiche.api.client.integration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class TokenApiImplTest {
+    private Dotenv dotenv;
+
+    @BeforeAll
+    public static void setUp() {
+        // Load environment variables
+        Dotenv dotenv = Dotenv
+                .configure()
+                .filename("TestConfig.env")
+                .load();
+    }
 }
