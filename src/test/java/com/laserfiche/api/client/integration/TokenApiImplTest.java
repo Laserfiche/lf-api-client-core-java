a52d108cb721e92037e9414ea223d6b716bbafd5
Jason Jia <jason.jia@laserfiche.com>
2022-06-02 10:14:58 -0400

    Move comment setup code into base class

--- src/test/java/com/laserfiche/api/client/integration/TokenApiImplTest.java
+++ src/test/java/com/laserfiche/api/client/integration/TokenApiImplTest.java
@@ -1,14 +1,8 @@
 package com.laserfiche.api.client.integration;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.laserfiche.api.client.model.AccessKey;
 import com.laserfiche.api.client.model.GetAccessTokenResponse;
 import com.laserfiche.api.client.oauth.TokenApiClient;
 import com.laserfiche.api.client.oauth.TokenApiImpl;
-import com.nimbusds.jose.jwk.JWK;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,32 +11,7 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class TokenApiImplTest {
-    private static String spKey;
-    private static AccessKey accessKey;
-
-    @BeforeAll
-    public static void setUp() {
-        // Load environment variables
-        Dotenv dotenv = Dotenv
-                .configure()
-                .filename("TestConfig.env")
-                .load();
-
-        // Read env variables
-        Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
-
-        String accessKeyStr = dotenv.get("DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY");
-        if (accessKeyStr == null) {
-            throw new RuntimeException("DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY environment variable is not set");
-        }
-        // Gson doesn't escape forward slash https://github.com/google/gson/issues/356
-        accessKeyStr = accessKeyStr.replace("\\\"", "\"");
-
-        accessKey = gson.fromJson(accessKeyStr, AccessKey.class);
-        spKey = dotenv.get("DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY");
-    }
-
+public class TokenApiImplTest extends BaseTest {
     @Test
     public void testGetAccessTokenAsync() {
         TokenApiClient client = new TokenApiImpl(accessKey.getDomain());
