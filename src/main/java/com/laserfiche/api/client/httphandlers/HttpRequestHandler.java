6e7f1d384bf70e5d400c8cd11158b75bf462f21f
Jason Jia <jason.jia@laserfiche.com>
2022-06-13 09:19:02 -0400

    Update the handler interface to use our own request and response classes

--- src/main/java/com/laserfiche/api/client/httphandlers/HttpRequestHandler.java
+++ src/main/java/com/laserfiche/api/client/httphandlers/HttpRequestHandler.java
@@ -1,12 +1,9 @@
 package com.laserfiche.api.client.httphandlers;
 
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
 import java.util.concurrent.CompletableFuture;
 
 public interface HttpRequestHandler {
-    CompletableFuture<BeforeSendResult> beforeSendAsync(Request.Builder request);
+    CompletableFuture<BeforeSendResult> beforeSendAsync(Request request);
 
     CompletableFuture<Boolean> afterSendAsync(Response response);
 }
