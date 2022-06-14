18c422c88d63f5c0959ad6be527580b9f97ed107
Jason Jia <jason.jia@laserfiche.com>
2022-06-14 17:12:19 -0400

    Add annotation

--- src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
+++ src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
@@ -3,6 +3,7 @@ package com.laserfiche.api.client.integration;
 import com.laserfiche.api.client.model.GetAccessTokenResponse;
 import com.laserfiche.api.client.oauth.TokenClient;
 import com.laserfiche.api.client.oauth.TokenClientImpl;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -10,7 +11,8 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TokenClientImplTest extends BaseTest {
-    public void getAccessTokenFromServicePrincipal() throws ExecutionException, InterruptedException {
+    @Test
+    public void getAccessTokenFromServicePrincipal_Success() throws ExecutionException, InterruptedException {
         TokenClient client = new TokenClientImpl("ca");
         CompletableFuture<GetAccessTokenResponse> future = client.getAccessTokenFromServicePrincipal(spKey, accessKey);
         GetAccessTokenResponse response = future.get();
