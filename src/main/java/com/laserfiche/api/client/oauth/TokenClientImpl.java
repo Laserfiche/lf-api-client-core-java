7df8c0a11edeaf6f06f0273c897d41383fe3b881
Jason Jia <jason.jia@laserfiche.com>
2022-06-15 10:54:39 -0400

    Remove unused import

--- src/main/java/com/laserfiche/api/client/oauth/TokenClientImpl.java
+++ src/main/java/com/laserfiche/api/client/oauth/TokenClientImpl.java
@@ -8,7 +8,6 @@ import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.ECDSASigner;
 import com.nimbusds.jose.jwk.ECKey;
 import okhttp3.OkHttpClient;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
