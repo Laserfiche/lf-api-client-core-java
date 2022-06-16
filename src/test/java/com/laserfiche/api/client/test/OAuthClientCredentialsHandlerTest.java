158855550560f02f82d6971300ece1dc50b9fad7
Jason Jia <jason.jia@laserfiche.com>
2022-06-16 14:44:43 -0400

    When receiving 200, should not retry

---
+++ src/test/java/com/laserfiche/api/client/test/OAuthClientCredentialsHandlerTest.java
@@ -0,0 +1,26 @@
+package com.laserfiche.api.client.test;
+
+import com.laserfiche.api.client.httphandlers.HttpRequestHandler;
+import com.laserfiche.api.client.httphandlers.OAuthClientCredentialsHandler;
+import com.laserfiche.api.client.httphandlers.Response;
+import com.laserfiche.api.client.integration.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class OAuthClientCredentialsHandlerTest extends BaseTest {
+    @Test
+    public void afterSendAsync_ShouldNotRetry() {
+        HttpRequestHandler handler = new OAuthClientCredentialsHandler(spKey, accessKey);
+        Response mockedResponse = mock(Response.class);
+        when(mockedResponse.status()).thenReturn((short)200);
+
+        // Request access token then simulate a 401
+        handler.afterSendAsync(mockedResponse).thenApply((shouldRetry) -> {
+            assertEquals(false, shouldRetry);
+            return null;
+        });
+    }
+}
