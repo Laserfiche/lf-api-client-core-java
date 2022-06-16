0adbca95320a74da9af53797f2c92e3d30a55303
Jason Jia <jason.jia@laserfiche.com>
2022-06-16 11:53:40 -0400

    Add test case for IO failure

--- src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
+++ src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
@@ -5,10 +5,11 @@ import com.laserfiche.api.client.oauth.TokenClient;
 import com.laserfiche.api.client.oauth.TokenClientImpl;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TokenClientImplTest extends BaseTest {
     @Test
@@ -19,5 +20,16 @@ public class TokenClientImplTest extends BaseTest {
 
         assertNotEquals(null, response);
         assertNotEquals(null, response.getAccessToken());
+    }
+
+    @Test
+    public void getAccessTokenFromServicePrincipal_IoError() throws ExecutionException, InterruptedException {
+        String incorrectDomain = accessKey.getDomain().replace("laserfiche", "lf");
+        TokenClient client = new TokenClientImpl(incorrectDomain);
+        CompletableFuture<GetAccessTokenResponse> future = client.getAccessTokenFromServicePrincipal(spKey, accessKey);
+
+        Exception exception = assertThrows(ExecutionException.class, future::get);
+
+        assertTrue(IOException.class.isAssignableFrom(exception.getCause().getCause().getClass()));
     }
 }
