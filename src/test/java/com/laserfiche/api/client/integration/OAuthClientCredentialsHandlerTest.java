ac73cd4e87880a8683a75c670e4c35f0911048a8
Jason Jia <jason.jia@laserfiche.com>
2022-06-16 14:41:56 -0400

    When receiving 401, should retry

--- src/test/java/com/laserfiche/api/client/integration/OAuthClientCredentialsHandlerTest.java
+++ src/test/java/com/laserfiche/api/client/integration/OAuthClientCredentialsHandlerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class OAuthClientCredentialsHandlerTest extends BaseTest {
     @Test
@@ -14,11 +16,26 @@ public class OAuthClientCredentialsHandlerTest extends BaseTest {
         HttpRequestHandler handler = new OAuthClientCredentialsHandler(spKey, accessKey);
         Request request = new RequestImpl();
 
+        // Request access token
         CompletableFuture<BeforeSendResult> future = handler.beforeSendAsync(request);
         BeforeSendResult result = future.get();
 
         assertNotEquals(null, result.getRegionalDomain());
         assertNotEquals(null, request.headers().get("Authorization"));
         assertNotEquals("", request.headers().get("Authorization"));
+    }
+
+    @Test
+    public void afterSendAsync_ShouldRetry() {
+        HttpRequestHandler handler = new OAuthClientCredentialsHandler(spKey, accessKey);
+        Response mockedResponse = mock(Response.class);
+        when(mockedResponse.status()).thenReturn((short)401);
+
+        // Request access token then simulate a 401
+        CompletableFuture<BeforeSendResult> tokenFuture = handler.beforeSendAsync(new RequestImpl());
+        tokenFuture.thenCompose(beforeSendResult -> handler.afterSendAsync(mockedResponse)).thenApply((shouldRetry) -> {
+            assertEquals(true, shouldRetry);
+            return null;
+        });
     }
 }
