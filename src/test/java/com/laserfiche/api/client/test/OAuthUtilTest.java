831728ae4c7a3343ddae9156067c7945b54df40f
Jason Jia <jason.jia@laserfiche.com>
2022-06-16 16:50:04 -0400

    Add test for createBearer

--- src/test/java/com/laserfiche/api/client/test/OAuthUtilTest.java
+++ src/test/java/com/laserfiche/api/client/test/OAuthUtilTest.java
@@ -1,12 +1,13 @@
 package com.laserfiche.api.client.test;
 
+import com.laserfiche.api.client.integration.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import static com.laserfiche.api.client.oauth.OAuthUtil.createBearer;
 import static com.laserfiche.api.client.oauth.OAuthUtil.getOAuthApiBaseUri;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class OAuthUtilTest {
+public class OAuthUtilTest extends BaseTest {
     @Test
     public void getOAuthApiBaseUri_ReturnDomain() {
         String domain = "domain";
@@ -21,5 +22,25 @@ public class OAuthUtilTest {
     @Test
     public void getOAuthApiBaseUri_RejectsEmptyString() {
         assertThrows(IllegalArgumentException.class, () -> getOAuthApiBaseUri(""));
+    }
+
+    @Test
+    public void createBearer_ReturnsCorrectJwt() {
+        String jwt = createBearer(spKey, accessKey);
+
+        assertNotNull(jwt);
+
+        String[] parts = jwt.split("[ .]");
+
+        assertNotNull(parts);
+        assertEquals(4, parts.length);
+        assertNotNull(parts[0]);
+        assertNotNull(parts[1]);
+        assertNotNull(parts[2]);
+        assertNotNull(parts[3]);
+        assertEquals("Bearer", parts[0]);
+        assertTrue(parts[1].length() > 0);
+        assertTrue(parts[2].length() > 0);
+        assertTrue(parts[3].length() > 0);
     }
 }
