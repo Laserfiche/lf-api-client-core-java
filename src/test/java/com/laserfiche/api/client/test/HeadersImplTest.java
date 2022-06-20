e5488140997a4779a67811579defeb0fca2215f2
Jason Jia <jason.jia@laserfiche.com>
2022-06-20 09:40:03 -0400

    Add skeleton unit tests to finish

---
+++ src/test/java/com/laserfiche/api/client/test/HeadersImplTest.java
@@ -0,0 +1,75 @@
+package com.laserfiche.api.client.test;
+
+import org.junit.jupiter.api.Test;
+
+public class HeadersImplTest {
+    @Test
+    public void append_ShouldAddHeaderWhenHeaderNameNotSet() {
+
+    }
+
+    @Test
+    public void append_ShouldAddHeaderWhenHeaderNameAlreadySet() {
+
+    }
+
+    @Test
+    public void append_ShouldNotSetHeaderIfNull() {
+
+    }
+
+    @Test
+    public void delete_ShouldRemoveHeader() {
+
+    }
+
+    @Test
+    public void delete_ShouldDoNothingWhenHeaderDoesNotExist() {
+
+    }
+
+    @Test
+    public void entries_ShouldReturnEmptyStringWhenNoHeadersSet() {
+
+    }
+
+    @Test
+    public void entries_ShouldReturnAllHeaders() {
+
+    }
+
+    @Test
+    public void get_ShouldReturnHeaderValue() {
+
+    }
+
+    @Test
+    public void get_ShouldReturnNullIfHeaderDoesNotExist() {
+
+    }
+
+    @Test
+    public void has_ShouldReturnFalseIfHeaderDoesNotExist() {
+
+    }
+
+    @Test
+    public void has_ShouldReturnTrueIfHeaderExists() {
+
+    }
+
+    @Test
+    public void keys_ShouldReturnAllKeys() {
+
+    }
+
+    @Test
+    public void set_ShouldSetHeader() {
+
+    }
+
+    @Test
+    public void values_ShouldReturnAllValues() {
+
+    }
+}
