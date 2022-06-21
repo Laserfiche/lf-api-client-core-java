c0957a0a530cb23f114ed063ad35a46e43c26e93
Steven Xu <steven.xu@laserfiche.com>
2022-06-21 16:23:55 -0400

    Added file exception handling for the JUnit test cases

--- src/test/java/com/laserfiche/api/client/integration/BaseTest.java
+++ src/test/java/com/laserfiche/api/client/integration/BaseTest.java
@@ -5,8 +5,12 @@ import com.google.gson.GsonBuilder;
 import com.laserfiche.api.client.model.AccessKey;
 import com.nimbusds.jose.jwk.JWK;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 import org.junit.jupiter.api.BeforeAll;
+import org.mockito.internal.matchers.Null;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Base64;
 
 public class BaseTest {
@@ -14,23 +18,38 @@ public class BaseTest {
     protected static AccessKey accessKey;
 
     @BeforeAll
-    public static void setUp() {
+    public static void setUp() throws DotenvException, NullPointerException {
         // Load environment variables
-        Dotenv dotenv = Dotenv
-                .configure()
-                .filename(".env")
-                .load();
+        try {
+            Dotenv dotenv = Dotenv
+                    .configure()
+                    .filename("TestConfig.env").load();
+            // Read env variables
+            Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
+            String accessKeyBase64 = dotenv.get("ACCESS_KEY");
+            //System.out.println(accessKeyBase64);
+            String accessKeyStr = decodeBase64(accessKeyBase64);
+            // Gson doesn't escape forward slash https://github.com/google/gson/issues/356
+            accessKeyStr = accessKeyStr.replace("\\\"", "\"");
 
-        // Read env variables
-        Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
-
-        String accessKeyBase64 = dotenv.get("ACCESS_KEY");
-        String accessKeyStr = decodeBase64(accessKeyBase64);
-        // Gson doesn't escape forward slash https://github.com/google/gson/issues/356
-        accessKeyStr = accessKeyStr.replace("\\\"", "\"");
-
-        accessKey = gson.fromJson(accessKeyStr, AccessKey.class);
-        spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+            accessKey = gson.fromJson(accessKeyStr, AccessKey.class);
+            spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+        } catch (DotenvException | NullPointerException e) {
+            if (e instanceof DotenvException) {
+                System.out.println("File not found");
+            }
+            if (e instanceof NullPointerException) {
+                System.out.println("Environment variables are not loaded properly from the Testconfig.env file");
+            }
+            else{
+                throw new RuntimeException("uncaught", e);
+            }
+        }
+        /*catch (DotenvException e) {
+            System.out.println("File not found");
+        }catch(NullPointerException e){
+            System.out.println("Environment variables are not loaded properly from the Testconfig.env file");
+        }*/
     }
 
     private static String decodeBase64(String encoded) {
