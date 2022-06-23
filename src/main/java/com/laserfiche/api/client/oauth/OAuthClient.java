f7f522529da91ee5b97932993d6777722cf20300
Jason Jia <jason.jia@laserfiche.com>
2022-06-23 14:40:05 -0400

    Move the retrofit info out of JavaDoc so it doesn't confuse non-contributors

--- src/main/java/com/laserfiche/api/client/oauth/OAuthClient.java
+++ src/main/java/com/laserfiche/api/client/oauth/OAuthClient.java
@@ -7,8 +7,6 @@ import java.util.concurrent.CompletableFuture;
 
 /**
  * NOTE: This interface is an implementation detail and could change in any time. Do not directly use it.
- * Specifies how the underlying HTTP client should work to request access token. See <a href="https://square.github.io/retrofit/">...</a>
- * for more details.
  */
 public interface OAuthClient {
     @FormUrlEncoded
@@ -25,4 +23,7 @@ public interface OAuthClient {
     @Headers("Accept: application/json")
     @POST("Token")
     CompletableFuture<GetAccessTokenResponse> refreshAccessToken(@Field("grant_type") String grantType, @Field("refresh_token") String refreshToken, @Header("Content-Type") String contentType, @Header("Authorization") String authorization);
+
+    // Those interface methods specifies how the HttpClient should be configured so it can correctly talk to the OAuth
+    // backend. For more details, see https://square.github.io/retrofit/
 }
