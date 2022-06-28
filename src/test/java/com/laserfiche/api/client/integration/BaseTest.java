638bcc57e05f43f5c3451eb965bffe066f87c230
Steven Xu <steven.xu@laserfiche.com>
2022-06-28 17:08:08 -0400

    modified base test

--- src/test/java/com/laserfiche/api/client/integration/BaseTest.java
+++ src/test/java/com/laserfiche/api/client/integration/BaseTest.java
@@ -10,35 +10,32 @@ import org.junit.jupiter.api.BeforeAll;
 import java.util.Base64;
 
 public class BaseTest {
-    protected static String spKey = System.getenv("ACCESS_KEY");
-
-    protected static String accessKeyBase64 = System.getenv("SERVICE_PRINCIPAL_KEY");
+    protected static String spKey;
     protected static AccessKey accessKey;
 
     @BeforeAll
     public static void setUp() {
-        System.out.println(spKey);
-        System.out.println(accessKeyBase64);
-        System.out.println(System.getProperties());
-        //if (spKey == null && accessKeyBase64 == null) {
-        // Load environment variables
-        Dotenv dotenv = Dotenv
-                .configure()
-                .filename(".env")
-                .load();
-        // Read env variables
-        Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
-
-        accessKeyBase64 = dotenv.get("ACCESS_KEY");
-        //}
+        spKey = System.getenv("ACCESS_KEY");;
+        String accessKeyBase64 = System.getenv("SERVICE_PRINCIPAL_KEY");
+        if (spKey == null && accessKeyBase64 == null) {
+            // Load environment variables
+            Dotenv dotenv = Dotenv
+                    .configure()
+                    .filename(".env")
+                    .load();
+            // Read env variable
+            accessKeyBase64 = dotenv.get("ACCESS_KEY");
+            spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+        }
         String accessKeyStr = decodeBase64(accessKeyBase64);
         // Gson doesn't escape forward slash https://github.com/google/gson/issues/356
         accessKeyStr = accessKeyStr.replace("\\\"", "\"");
 
+        Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
         accessKey = gson.fromJson(accessKeyStr, AccessKey.class);
-        spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-        System.out.println(System.getProperties());
+        //spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+        //dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        //System.out.println(System.getProperties());
 
     }
 
