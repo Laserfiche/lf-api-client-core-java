1afbe342b72353a0ba1c5c28c1732b24e5992dbc
Jason Jia <jason.jia@laserfiche.com>
2022-08-11 17:24:14 -0400

    Implement an object mapper using Jackson

--- src/main/java/com/laserfiche/api/client/oauth/TokenClientObjectMapper.java
+++ src/main/java/com/laserfiche/api/client/oauth/TokenClientObjectMapper.java
@@ -1,2 +1,40 @@
-package com.laserfiche.api.client.oauth;public class TokenClientObjectMapper {
+package com.laserfiche.api.client.oauth;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
+import kong.unirest.GenericType;
+import kong.unirest.ObjectMapper;
+
+import java.io.IOException;
+
+public class TokenClientObjectMapper implements ObjectMapper {
+    private com.fasterxml.jackson.databind.ObjectMapper jacksonMapper;
+
+    public TokenClientObjectMapper() {
+        jacksonMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        jacksonMapper.configure(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS, false);
+    }
+
+    @Override
+    public <T> T readValue(String s, Class<T> aClass) {
+        try {
+            return jacksonMapper.readValue(s, aClass);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> T readValue(String value, GenericType<T> genericType) {
+        return ObjectMapper.super.readValue(value, genericType);
+    }
+
+    @Override
+    public String writeValue(Object o) {
+        try {
+            return jacksonMapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
