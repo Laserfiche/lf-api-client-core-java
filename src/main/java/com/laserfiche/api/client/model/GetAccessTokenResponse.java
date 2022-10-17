a446f629446850136eea73036855685dda427b34
Steven Xu <steven.xu@laserfiche.com>
2022-10-17 17:02:25 -0400

    add private fields + accessors

--- src/main/java/com/laserfiche/api/client/model/GetAccessTokenResponse.java
+++ src/main/java/com/laserfiche/api/client/model/GetAccessTokenResponse.java
@@ -1,17 +1,120 @@
 package com.laserfiche.api.client.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.Objects;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-10-17T14:51:42.906908300-04:00[America/New_York]")
 public class GetAccessTokenResponse {
-  @JsonProperty("access_token")
-  public String accessToken;
+    @JsonProperty("access_token")
+    private String accessToken = null;
 
-  @JsonProperty("expires_in")
-  public Integer expiresIn;
+    @JsonProperty("expires_in")
+    private Integer expiresIn = null;
 
-  @JsonProperty("token_type")
-  public String tokenType;
+    @JsonProperty("token_type")
+    private String tokenType = null;
 
-  @JsonProperty("refresh_token")
-  public String refreshToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken = null;
+
+    public GetAccessTokenResponse accessToken(String accessToken) {
+        this.accessToken = accessToken;
+        return this;
+    }
+
+    @Schema(description = "")
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public GetAccessTokenResponse expiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+        return this;
+    }
+
+    @Schema(description = "")
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public GetAccessTokenResponse tokenType(String tokenType) {
+        this.tokenType = tokenType;
+        return this;
+    }
+
+    @Schema(description = "")
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public GetAccessTokenResponse refreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+        return this;
+    }
+
+    @Schema(description = "")
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GetAccessTokenResponse getAccessTokenResponse = (GetAccessTokenResponse) o;
+        return Objects.equals(this.accessToken, getAccessTokenResponse.accessToken) &&
+                Objects.equals(this.expiresIn, getAccessTokenResponse.expiresIn) &&
+                Objects.equals(this.tokenType, getAccessTokenResponse.tokenType) &&
+                Objects.equals(this.refreshToken, getAccessTokenResponse.refreshToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accessToken, expiresIn, tokenType, refreshToken);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class GetAccessTokenResponse {\n");
+
+        sb.append("    accessToken: ").append(toIndentedString(accessToken)).append("\n");
+        sb.append("    expiresIn: ").append(toIndentedString(expiresIn)).append("\n");
+        sb.append("    tokenType: ").append(toIndentedString(tokenType)).append("\n");
+        sb.append("    refreshToken: ").append(toIndentedString(refreshToken)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
 }
--- src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
+++ src/test/java/com/laserfiche/api/client/integration/TokenClientImplTest.java
@@ -14,18 +14,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class TokenClientImplTest extends BaseTest {
     @Test
     void getAccessTokenFromServicePrincipal_Success() {
-        TokenClient client = new TokenClientImpl(accessKey.domain);
+        TokenClient client = new TokenClientImpl(accessKey.getDomain());
         CompletableFuture<GetAccessTokenResponse> future = client.getAccessTokenFromServicePrincipal(spKey, accessKey);
         GetAccessTokenResponse response = future.join();
 
         assertNotEquals(null, response);
-        assertNotEquals(null, response.accessToken);
+        assertNotEquals(null, response.getAccessToken());
     }
 
     @Test
     void getAccessTokenFromServicePrincipal_InvalidAccessKey() {
-        TokenClient client = new TokenClientImpl(accessKey.domain);
-        accessKey.clientId = "wrong client ID";
+        TokenClient client = new TokenClientImpl(accessKey.getDomain());
+        accessKey.setClientId("wrong client ID");
         CompletableFuture<GetAccessTokenResponse> future = client.getAccessTokenFromServicePrincipal(spKey, accessKey);
 
         Exception exception = assertThrows(RuntimeException.class, future::join);
@@ -34,7 +34,7 @@ class TokenClientImplTest extends BaseTest {
 
     @Test
     void getAccessTokenFromServicePrincipal_IoError() {
-        String incorrectDomain = accessKey.domain.replace("laserfiche", "lf");
+        String incorrectDomain = accessKey.getDomain().replace("laserfiche", "lf");
         TokenClient client = new TokenClientImpl(incorrectDomain);
         CompletableFuture<GetAccessTokenResponse> future = client.getAccessTokenFromServicePrincipal(spKey, accessKey);
 
