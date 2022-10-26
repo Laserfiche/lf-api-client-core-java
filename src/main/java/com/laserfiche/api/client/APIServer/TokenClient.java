f39e362669f56e9549efd4d1b8e0d24059f5e016
Steven Xu <steven.xu@laserfiche.com>
2022-10-26 10:26:27 -0400

    added documentation and ApiException constructor

--- src/main/java/com/laserfiche/api/client/APIServer/TokenClient.java
+++ src/main/java/com/laserfiche/api/client/APIServer/TokenClient.java
@@ -6,5 +6,11 @@ import java.util.concurrent.CompletableFuture;
 
 public interface TokenClient {
 
+    /**
+     * @param repoId Repository name
+     * @param body   Request body that contains username, passward and grant type
+     * @return Create an access token successfuly.
+     */
+
     CompletableFuture<SessionKeyInfo> createAccessToken(String repoId, CreateConnectionRequest body);
 }
