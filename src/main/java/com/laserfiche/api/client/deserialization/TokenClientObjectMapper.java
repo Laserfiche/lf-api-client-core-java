bf858d56c2aeed3e40cac331bc5496a4236a451e
Jason Jia <101835056+lf-jason-jia@users.noreply.github.com>
2022-12-06 09:22:08 -0500

    Add custom deserializer (#52)
    * Make it functionality fit for both client core and repository client
    
    * Bump patch as there's no functionality difference

--- src/main/java/com/laserfiche/api/client/deserialization/TokenClientObjectMapper.java
+++ src/main/java/com/laserfiche/api/client/deserialization/TokenClientObjectMapper.java
@@ -4,17 +4,23 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import kong.unirest.GenericType;
 import kong.unirest.ObjectMapper;
+import org.threeten.bp.OffsetDateTime;
 
 import java.io.IOException;
 
 public class TokenClientObjectMapper implements ObjectMapper {
-    private com.fasterxml.jackson.databind.ObjectMapper jacksonMapper;
+    private final com.fasterxml.jackson.databind.ObjectMapper jacksonMapper;
 
     public TokenClientObjectMapper() {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(OffsetDateTime.class, new OffsetDateTimeDeserializer());
+
         jacksonMapper = JsonMapper
                 .builder()
+                .addModule(module)
                 .disable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
