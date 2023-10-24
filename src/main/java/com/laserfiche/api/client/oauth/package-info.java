05f831d51a672ebe82224bc1b846532c5fb9fa1a
Samad Paydar <samad.paydar@laserfiche.com>
2023-10-24 15:45:11 -0400

    Add . to the end of copyright header.

--- src/main/java/com/laserfiche/api/client/oauth/package-info.java
+++ src/main/java/com/laserfiche/api/client/oauth/package-info.java
@@ -1,4 +1,4 @@
-// Copyright (c) Laserfiche
+// Copyright (c) Laserfiche.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 /**
  * Provides classes to get access tokens for Laserfiche Cloud.
